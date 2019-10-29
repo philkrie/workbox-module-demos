8770f22fb34f9314ce4e047b464f4e741107aa61
philkrie <philkrie@google.com>
2019-10-29 10:28:45 -0700

    changed request shape for strategies demo

--- src/workbox-strategies/sw.js
+++ src/workbox-strategies/sw.js
@@ -11,35 +11,35 @@ const {strategies} = workbox;
 
 //Needs a Request object, which I understand is provided by event.request. What am i missing?
 self.addEventListener('fetch', (event) => {
-  console.log(new URL(event.request.url).pathname);
+  const request = event.request;
   switch (new URL(event.request.url).pathname) {
     case '/public/cache-only-empty-cache.txt':
       const cacheOnlyEmpty = new workbox.strategies.CacheOnly();
-      event.respondWith(cacheOnlyEmpty.handle({request: event.request, event: event}));
+      event.respondWith(cacheOnlyEmpty.handle({event, request}));
       break;
     case '/public/cache-only-populated-cache':
       const cacheOnlyPopulated = new workbox.strategies.CacheOnly();
-      event.respondWith(cacheOnlyPopulated.handle({request: event.request, event: event}));
+      event.respondWith(cacheOnlyPopulated.handle({event, request}));
       break;
     case '/public/cache-first.txt':
       const cacheFirst = new workbox.strategies.CacheFirst();
-      event.respondWith(cacheFirst.handle({request: event.request, event: event}));
+      event.respondWith(cacheFirst.handle({event, request}));
       break;
     case '/public/network-only.txt':
       const networkOnly = new workbox.strategies.NetworkOnly();
-      event.respondWith(networkOnly.handle({request: event.request, event: event}));
+      event.respondWith(networkOnly.handle({event, request}));
       break;
     case '/public/network-first.txt':
       const networkFirst = new workbox.strategies.NetworkFirst();
-      event.respondWith(networkFirst.handle({request: event.request, event: event}));
+      event.respondWith(networkFirst.handle({event, request}));
       break;
     case '/public/network-first-404.txt':
       const networkFirstInvalid = new workbox.strategies.NetworkFirst();
-      event.respondWith(networkFirstInvalid.handle({request: event.request, event: event}));
+      event.respondWith(networkFirstInvalid.handle({event, request}));
       break;
     case '/public/stale-while-revalidate.txt':
       const staleWhileRevalidate = new workbox.strategies.StaleWhileRevalidate();
-      event.respondWith(staleWhileRevalidate.handle({request: event.request, event: event}));
+      event.respondWith(staleWhileRevalidate.handle({event, request}));
       break;
   }
 });
